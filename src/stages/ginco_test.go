a47da4774f4330bb1ab8ba5ddb975bf07eadc793
Morten Trudsø <trudso@gmail.com>
2025-03-22 08:29:32 +0100

    Added package parsing test with multiple models

--- src/stages/ginco_test.go
+++ src/stages/ginco_test.go
@@ -106,3 +106,38 @@ func TestParseSimplePackage(t *testing.T) {
 	assert.Equal(t, 1, len(pkg.Models))
 	assert.Equal(t, len(inputTest), nextIdx) 
 }
+
+func TestParseMultiModelPackage(t *testing.T) {
+	inputTest := `package people {
+		@changeset
+		model Person {
+			fields {
+				=1 firstName string
+				=1 lastName string
+				=1 birthDate date
+				=? deathDate date
+				=1 residence Address
+				=? billingAddress Address
+				-? partner Person
+			}
+		}
+
+		@changeset
+		model Address {
+			fields {
+				=1 streetName string
+				=1 streetNumber string
+				=1 city string
+				=1 zipCode string
+			}
+		}
+	}`
+
+	pkg, nextIdx, err := parsePackage( inputTest, 0)
+	assert.NoError(t, err)
+	assert.NotNil(t, pkg )
+	assert.Equal(t, 2, len(pkg.Models))
+	assert.Equal(t, len(inputTest), nextIdx) 
+}
+
+
